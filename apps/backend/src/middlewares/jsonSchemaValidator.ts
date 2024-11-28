7e1abb181970e763099a0ac48dd4e4e2caac5c7d
Naval.dourbi <parneet.kaur@ondc.org>
2024-11-28 17:54:22 +0530

    fix/payload confirm to update

--- apps/backend/src/middlewares/jsonSchemaValidator.ts
+++ apps/backend/src/middlewares/jsonSchemaValidator.ts
@@ -75,7 +75,7 @@ export const jsonSchemaValidator = <T extends Domain>({
       }
 
 
-
+      console.log("intit",action,JSON.stringify(req.body))
 
       switch (domain) {
         case "services":
